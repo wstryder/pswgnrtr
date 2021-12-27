e6a694ca77d98d86f1aa4225a0827650af1fe3da
Ozfir Izmgzoz <late@sapatti.fi>
2021-12-27 23:12:03 +0200

    Create pswgnrtr.pl

---
+++ pswgnrtr.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+# A good password is at least 20 characters long.
+# A random generator is the only way to get a good password.
+# Nolicense.
+#
+
+my $len = $ARGV[0];
+if (not defined $len) {
+	$len = 20;
+}
+my $pwd;
+
+# excluding ambiguos characters like "iIl1O0o"
+# there are some special characters I can never remember how to get
+# them from my keyboard, so these are my favorites.
+my @chars = ('A'..'Z','a'..'k','m','n','p'..'z','2'..'9','!','#','%','&','/','(',')','=','?','*','-','_');
+my $range = $#chars + 1;
+for (1..$len) {
+   $pwd .= $chars[int(rand($range))];
+}
+print "$pwd\n";
